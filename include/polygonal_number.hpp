e91e64260cfe80b9fdde1514ea9e3e2e5860c7b7
Paul Jungeblut <paul.jungeblut@gmail.com>
2019-10-22 00:02:48 +0200

    Adding code for polygonal numbers and project euler, problem 45.

---
+++ include/polygonal_number.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+/**
+ * Implementations to generate polygonal numbers.
+ */
+
+namespace ntlib {
+
+/**
+ * Computes the n-th triangle number.
+ *
+ * @tparam NumberType Integral data type used for natural numbers.
+ *
+ * @param n Which triangle number to gnerate.
+ * @return The n-th triangle number.
+ */
+template<typename NumberType>
+NumberType triangle_number(NumberType n) {
+  return n * (n + 1) / 2;
+}
+
+/**
+ * Computes the n-th square number.
+ *
+ * @tparam NumberType Integral data type used for natural numbers.
+ *
+ * @param n Which square number to gnerate.
+ * @return The n-th square number.
+ */
+template<typename NumberType>
+NumberType square_number(NumberType n) {
+  return n * n;
+}
+
+/**
+ * Computes the n-th pentagonal number.
+ *
+ * @tparam NumberType Integral data type used for natural numbers.
+ *
+ * @param n Which pentagonal number to gnerate.
+ * @return The n-th pentagonal number.
+ */
+template<typename NumberType>
+NumberType pentagonal_number(NumberType n) {
+  return n * (3 * n - 1) / 2;
+}
+
+/**
+ * Computes the n-th hexagonal number.
+ *
+ * @tparam NumberType Integral data type used for natural numbers.
+ *
+ * @param n Which hexagonal number to gnerate.
+ * @return The n-th hexagonal number.
+ */
+template<typename NumberType>
+NumberType hexagonal_number(NumberType n) {
+  return n * (2 * n - 1);
+}
+
+}
