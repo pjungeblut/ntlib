f1a927d53cd62dcca065e47b050b9fdc56da0838
Paul Jungeblut <paul.jungeblut@gmail.com>
2019-10-27 23:48:42 +0100

    Making a parameter const.

--- include/prime_decomposition.hpp
+++ include/prime_decomposition.hpp
@@ -21,39 +21,41 @@
 #include "include/sieve_eratosthenes.hpp"
 
 namespace ntlib {
-  /**
-   * Computes the prime decomposition of a number.
-   * Takes a list of primes used as possible prime factors.
-   *
-   * @param n The number to decompose.
-   * @param factors The prime factors.
-   * @param primes List of primes used as possible prime factors.
-   *               Must contain all prime factors p, such that p*p <= n to work
-   *               correctly.
-   */
-  template<typename NumberType>
-  void prime_decomposition_list(NumberType n, std::map<NumberType, NumberType> &factors,
-      std::vector<NumberType> &primes) {
-    for (std::size_t i = 0; i < primes.size() && primes[i] * primes[i] <= n; ++i) {
-      while (n % primes[i] == 0) {
-        n /= primes[i];
-        factors[primes[i]]++;
-      }
+  
+/**
+ * Computes the prime decomposition of a number.
+ * Takes a list of primes used as possible prime factors.
+ *
+ * @param n The number to decompose.
+ * @param factors The prime factors.
+ * @param primes List of primes used as possible prime factors.
+ *               Must contain all prime factors p, such that p*p <= n to work
+ *               correctly.
+ */
+template<typename NumberType>
+void prime_decomposition_list(NumberType n, std::map<NumberType, NumberType> &factors,
+    const std::vector<NumberType> &primes) {
+  for (std::size_t i = 0; i < primes.size() && primes[i] * primes[i] <= n; ++i) {
+    while (n % primes[i] == 0) {
+      n /= primes[i];
+      factors[primes[i]]++;
     }
-    if (n != 1) factors[n]++;
   }
+  if (n != 1) factors[n]++;
+}
 
-  /**
-   * Computes the prime decomposition of a number.
-   *
-   * @param n The number to decompose.
-   * @param factors The prime factors.
-   */
-  template<typename NumberType>
-  void prime_decomposition(NumberType n, std::map<NumberType, NumberType> &factors) {
-    NumberType iroot = isqrt(n);
-    std::vector<NumberType> primes;
-    sieve_eratosthenes_list_segmented(iroot, primes);
-    prime_decomposition_list(n, factors, primes);
-  }
+/**
+ * Computes the prime decomposition of a number.
+ *
+ * @param n The number to decompose.
+ * @param factors The prime factors.
+ */
+template<typename NumberType>
+void prime_decomposition(NumberType n, std::map<NumberType, NumberType> &factors) {
+  NumberType iroot = isqrt(n);
+  std::vector<NumberType> primes;
+  sieve_eratosthenes_list_segmented(iroot, primes);
+  prime_decomposition_list(n, factors, primes);
+}
+
 }
