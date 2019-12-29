471551ba8548dde170e0904d11bf3a0444fa2075
Paul Jungeblut <paul.jungeblut@gmail.com>
2019-12-29 19:44:46 +0100

    Cleaning up cmake files.

--- build/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Activate globbing.
-shopt -s extglob
-
-# Remove all but the listed files in the directory and subdirectories.
-rm -rf !(clean.sh|.gitignore|..|.)
--- install.sh
+++ install.sh
@@ -4,14 +4,16 @@
 
 CUR_DIR=`pwd`
 
-# Installs into the home directory unless another location prefix is specified.
-NTLIB_INSTALL_PREFIX=${HOME}
+# Installs the header files to the given location.
 if [ $# -ge 1 ]; then
   NTLIB_INSTALL_PREFIX=${1}
+else
+  echo "ERROR: Specify a directory to install into."
+  exit 1
 fi
 
 # Get absolute path name of installation directory.
-mkdir -p "${NTLIB_INSTALL_PREFIX}" 2> /dev/null
+mkdir -p "${NTLIB_INSTALL_PREFIX}/include/ntlib" 2> /dev/null
 cd "${NTLIB_INSTALL_PREFIX}" > /dev/null 2>&1
 if [ $? != 0 ]; then
   echo "ERROR: Directory 'S{NTLIB_INSTALL_PREFIX}' does not exist nor could be created."
@@ -23,65 +25,20 @@ fi
 
 echo "Library will be installed in '${NTLIB_INSTALL_PREFIX}'."
 
-# Get the directory name the script is executed in.
-cd "${CUR_DIR}"
-OLD_DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd "${OLD_DIR}"
-OLD_DIR=`pwd`
-
-# Change into the build directory.
-cd build
-if [ $? != 0 ]; then
-  exit 1
-fi
-
-# Clean up build directory.
-./clean.sh
-if [ $? != 0 ]; then
-  exit 1
-fi
-
-# Run cmake.
-cmake -DCMAKE_INSTALL_PREFIX="${NTLIB_INSTALL_PREFIX}" ..
-if [ $? != 0 ]; then
-  echo "ERROR: cmake build failed."
-  exit 1
-fi
-
-# Run make.
-make ntlib
-if [ $? != 0 ]; then
-  echo "ERROR: Build failed."
-  exit 1
-fi
-
 # Removing old files.
 echo "Removing old files."
-echo "rm -rf '${NTLIB_INSTALL_PREFIX}/include/ntlib/*'"
-rm -rf "${NTLIB_INSTALL_PREFIX}/include/ntlib/*"
+rm -rf "${NTLIB_INSTALL_PREFIX}/include/ntlib"
 if [ $? != 0 ]; then
   echo "WARNING: Could not remove old header files."
 fi
-echo "rm -f '${NTLIB_INSTALL_PREFIX}/lib/libntlib*'"
-rm -f "${NTLIB_INSTALL_PREFIX}/lib/libntlib"
-if [ $? != 0 ]; then
-  echo "WARNING: Could not remove old library files."
-fi
 
 # Install library.
-make install
+cd ${CUR_DIR}
+cp -r ./include "${NTLIB_INSTALL_PREFIX}/include/ntlib"
 if [ $? != 0 ]; then
   echo "ERROR: Installation failed."
   exit 1
 fi
 
-cd ..
-
-if [ "`pwd`" != "${OLD_DIR}" ]; then
-  echo "ERROR: We are not in the original directory ${OLD_DIR} now."
-  exit 1
-fi
-
 echo "SUCCESS: ntlib was installed successfully."
 echo "The ntlib include files are located in '${NTLIB_INSTALL_PREFIX}/include'."
-echo "The library files are located in '${NTLIB_INSTALL_PREFIX}/lib'."
