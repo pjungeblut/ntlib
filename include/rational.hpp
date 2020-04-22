fb0a79ad5956eeaa4bc76fd2bc70c0bd1311a6e6
Paul Jungeblut <paul.jungeblut@gmail.com>
2020-04-22 22:32:45 +0200

    Adding operators to rational class and adding tests.

--- include/rational.hpp
+++ include/rational.hpp
@@ -4,6 +4,9 @@
  * Implementations to easily work with rational numbers.
  */
 
+#include <cassert>
+#include <string>
+
 #include "base.hpp"
 #include "integral.hpp"
 
@@ -14,19 +17,345 @@ namespace ntlib {
  */
 template<Integral I>
 struct rational {
-  I numerator;
-  I denominator;
-
   /**
    * Construct rational from a numerator and a denominator.
    *
    * @param n The numerator.
    * @param d The denominator. Defaults to 1.
    */
-  rational(I n, I d = 1) {
+  rational(I n = 0, I d = 1) {
+    assert(d != 0);
+
     I f = gcd(n, d);
     numerator = n / f;
     denominator = d / f;
+
+    if (denominator < 0) {
+      denominator *= -1;
+      numerator *= -1;
+    }
+  }
+
+  /**
+   * Returns the numerator.
+   *
+   * @return The numerator.
+   */
+  I get_numerator() {
+    return numerator;
+  }
+
+  /**
+   * Returns the denominator.
+   *
+   * @return The denominator.
+   */
+  I get_denominator() {
+    return denominator;
+  }
+
+  /**
+   * Creates a string "a/b" with reduced a and b.
+   *
+   * @return A string representation.
+   */
+  std::string to_string() const {
+    return std::to_string(numerator) + "/" + std::to_string(denominator);
+  }
+
+  /**
+   * Stream output.
+   *
+   * @param os The out stream.
+   * @param bu The rational to print.
+   * @return The out stream.
+   */
+  friend std::ostream& operator<<(std::ostream &os, const rational &r) {
+    os << r.to_string();
+    return os;
+  }
+
+  /**
+   * Equal operator.
+   *
+   * @param a The left hand side to compare.
+   * @param b The right hand side to compare.
+   * @return True, if and only if a == b.
+   */
+  friend bool operator==(const rational &a, const rational &b) {
+    return a.numerator == b.numerator && a.denominator == b.denominator;
+  }
+
+  /**
+   * Unequal operator.
+   *
+   * @param a The left hand side to compare.
+   * @param b The right hand side to compare.
+   * @return True, if and only if a != b.
+   */
+  friend bool operator!=(const rational &a, const rational &b) {
+    return !(a == b);
+  }
+
+  /**
+   * Smaller than operator.
+   *
+   * @param a The left hand side to compare.
+   * @param b The right hand side to compare.
+   * @return True, if and only if a < b.
+   */
+  friend bool operator<(const rational &a, const rational &b) {
+    return a.numerator * b.denominator < b.numerator * a.denominator;
+  }
+
+  /**
+   * Greater than operator.
+   *
+   * @param a The left hand side to compare.
+   * @param b The right hand side to compare.
+   * @return True, if and only if a > b.
+   */
+  friend bool operator>(const rational &a, const rational &b) {
+    return b < a;
+  }
+
+  /**
+   * Less than or equal operator.
+   *
+   * @param a The left hand side to compare.
+   * @param b The right hand side to compare.
+   * @return True, if and only if a <= b.
+   */
+  friend bool operator<=(const rational &a, const rational &b) {
+    return a < b || a == b;
+  }
+
+  /**
+   * Greather than or equal operator.
+   *
+   * @param a The left hand side to compare.
+   * @param b The right hand side to compare.
+   * @return True, if and only if a >= b.
+   */
+  friend bool operator>=(const rational &a, const rational &b) {
+    return a > b || a == b;
+  }
+
+  /**
+   * Unary minus to negate value.
+   *
+   * @return The rational with the same absolute value but opposite sign.
+   */
+  friend rational operator-(const rational &a) {
+    rational neg = a;
+    a.numerator *= -1;
+    return neg;
+  }
+
+  /**
+   * Addition for rationals.
+   *
+   * @param a The first summand.
+   * @param b The second summand.
+   * @return The sum of a and b.
+   */
+  friend rational operator+(const rational &a, const rational &b) {
+    rational sum(0);
+    add(a, b, sum);
+    return sum;
+  }
+
+  /**
+   * Adds another rational to the current one.
+   *
+   * @param other The other summand.
+   * @return Reference to the current rational containing the sum of its
+   *         previous value and other.
+   */
+  rational& operator+=(const rational &other) {
+    add(*this, other, *this);
+    return *this;
+  }
+
+  /**
+   * Subtraction for rationals.
+   *
+   * @param a The minuend.
+   * @param b The subtrahend.
+   * @return The difference of a and b.
+   */
+  friend rational operator-(const rational &a, const rational &b) {
+    rational difference;
+    subtract(a, b, difference);
+    return difference;
+  }
+
+  /**
+   * Subtracts another rational from the current one.
+   *
+   * @param other The subtrahend.
+   * @return Reference to the current rational containing the difference of its
+   *         previous value and other.
+   */
+  rational& operator-=(const rational &other) {
+    subtract(*this, other, *this);
+    return *this;
+  }
+
+  /**
+   * Multiplication for rationals.
+   *
+   * @param a The first factor.
+   * @param b The second factor.
+   * @return The product of a and b.
+   */
+  friend rational operator*(const rational &a, const rational &b) {
+    rational product;
+    multiply(a, b, product);
+    return product;
+  }
+
+  /**
+   * Multiplies another rational to the current one.
+   *
+   * @param other The other factor.
+   * @return Reference to the current rational containing the product of its
+   *         previous value and other.
+   */
+  rational& operator*=(const rational &other) {
+    multiply(*this, other, *this);
+    return *this;
+  }
+
+  /**
+   * Division for rationals.
+   *
+   * @param a The dividend.
+   * @param b The divisor.
+   * @return The quotient of a and b.
+   */
+  friend rational operator/(const rational &a, const rational &b) {
+    assert(b != 0);
+    rational quotient;
+    divide(a, b, quotient);
+    return quotient;
+  }
+
+  /**
+   * Divides another rational from the current one.
+   *
+   * @param other The divisor.
+   * @return Reference to the current rational containing the quotient of its
+   *         previous value and other.
+   */
+  rational& operator/=(const rational &other) {
+    assert(other != 0);
+    divide(*this, other, *this);
+    return *this;
+  }
+
+  /**
+   * Cast to bool.
+   */
+  explicit operator bool() const {
+    return numerator != 0;
+  }
+
+private:
+  /**
+   * Numerator and denominator.
+   */
+  I numerator;
+  I denominator;
+
+  /**
+   * Adds two rationals into a third.
+   * Parameters a and c may be the same for a += b.
+   *
+   * @param a The first summand.
+   * @param b The second summand.
+   * @param c The sum a + b.
+   */
+  static void add(const rational &a, const rational &b, rational &c) {
+    I lcd = lcm(a.denominator, b.denominator);
+    I fa = lcd / a.denominator;
+    I fb = lcd / b.denominator;
+    I numerator = fa * a.numerator + fb * b.numerator;
+    I denominator = lcd;
+    I g = gcd(numerator, denominator);
+    if (g != 1) {
+      numerator /= g;
+      denominator /= g;
+    }
+    c.numerator = numerator;
+    c.denominator = denominator;
+  }
+
+  /**
+   * Subtracts two rationals into a third.
+   * Parameters a and c may be the same for a -= b.
+   *
+   * @param a The minuend.
+   * @param b The subtrahend.
+   * @param c The difference a - b.
+   */
+  static void subtract(const rational &a, const rational &b, rational &c) {
+    I lcd = lcm(a.denominator, b.denominator);
+    I fa = lcd / a.denominator;
+    I fb = lcd / b.denominator;
+    I numerator = fa * a.numerator - fb * b.numerator;
+    I denominator = lcd;
+    I g = gcd(numerator, denominator);
+    if (g != 1) {
+      numerator /= g;
+      denominator /= g;
+    }
+    c.numerator = numerator;
+    c.denominator = denominator;
+  }
+
+  /**
+   * Multiplies two rationals into a third.
+   * Parameters a and c may be the same for a *= b.
+   *
+   * @param a The first factor.
+   * @param b The second factor.
+   * @param c The product a * b.
+   */
+  static void multiply(const rational &a, const rational &b, rational &c) {
+    I numerator = a.numerator * b.numerator;
+    I denominator = a.denominator * b.denominator;
+    I g = gcd(numerator, denominator);
+    if (g != 1) {
+      numerator /= g;
+      denominator /= g;
+    }
+    c.numerator = numerator;
+    c.denominator = denominator;
+  }
+
+  /**
+   * Divides two rationals into a third.
+   * Parameters a and c may be the same for a /= b.
+   *
+   * @param a The dividend.
+   * @param b The divisor. Must not be zero.
+   * @param c The quotient a / b.
+   */
+  static void divide(const rational &a, const rational &b, rational &c) {
+    I numerator = a.numerator * b.denominator;
+    I denominator = a.denominator * b.numerator;
+    I g = gcd(numerator, denominator);
+    if (g != 1) {
+      numerator /= g;
+      denominator /= g;
+    }
+    if (denominator < 0) {
+      numerator *= -1;
+      denominator *= -1;
+    }
+    c.numerator = numerator;
+    c.denominator = denominator;
   }
 };
 
---
+++ test/rational.cpp
@@ -0,0 +1,113 @@
+#include <gtest/gtest.h>
+
+#include "rational.hpp"
+
+TEST(Initialization, ConstructionByValue) {
+  // General tests.
+  EXPECT_EQ(ntlib::rational(5), 5);
+  EXPECT_EQ(ntlib::rational(6, 2), 3);
+
+  // Invalid input.
+  EXPECT_DEATH(ntlib::rational(5, 0), "");
+
+  // Negative denominator.
+  EXPECT_EQ(ntlib::rational(4, -2), -2);
+}
+
+TEST(Output, StringRepresentation) {
+  ntlib::rational a(5, 2);
+  EXPECT_EQ(a.to_string(), "5/2");
+  ntlib::rational b(10, 2);
+  EXPECT_EQ(b.to_string(), "5/1");
+}
+
+TEST(Operators, Comparison) {
+  ntlib::rational a(1, 2);
+  ntlib::rational b(2, 1);
+  ntlib::rational c(2, 4);
+
+  EXPECT_TRUE(a == a);
+  EXPECT_FALSE(a == b);
+  EXPECT_TRUE(a == c);
+  EXPECT_FALSE(b == c);
+
+  EXPECT_FALSE(a != a);
+  EXPECT_TRUE(a != b);
+  EXPECT_FALSE(a != c);
+  EXPECT_FALSE(b != b);
+
+  EXPECT_TRUE(a < b);
+  EXPECT_FALSE(a < a);
+
+  EXPECT_TRUE(b > a);
+  EXPECT_FALSE(a > a);
+
+  EXPECT_TRUE(a <= a);
+  EXPECT_TRUE(a <= b);
+
+  EXPECT_TRUE(a >= a);
+  EXPECT_FALSE(a >= b);
+}
+
+TEST(Arithmetic, Addition) {
+  ntlib::rational a(5, 2);
+  ntlib::rational b(7, 2);
+  ntlib::rational z(0, 2);
+
+  EXPECT_EQ(a + z, a);
+  EXPECT_EQ(a + b, 6);
+  EXPECT_EQ(b + a, 6);
+  a += b;
+  EXPECT_EQ(a, 6);
+  a += 5;
+  EXPECT_EQ(a, 11);
+  b += z;
+  EXPECT_EQ(b, b);
+}
+
+TEST(Arithmetic, Subtraction) {
+  ntlib::rational a(5, 2);
+  ntlib::rational b(7, 2);
+  ntlib::rational z(0, 2);
+
+  EXPECT_EQ(a - z, a);
+  EXPECT_EQ(a - b, -1);
+  EXPECT_EQ(b - a, 1);
+  a -= b;
+  EXPECT_EQ(a, -1);
+  a -= 5;
+  EXPECT_EQ(a, -6);
+  b -= z;
+  EXPECT_EQ(b, b);
+}
+
+TEST(Arithmetic, Multiplication) {
+  ntlib::rational a(5, 2);
+  ntlib::rational b(4, 5);
+  ntlib::rational z(0, 2);
+
+  EXPECT_EQ(a * z, 0);
+  EXPECT_EQ(a * b, b * a);
+  EXPECT_EQ(a * b, 2);
+  a *= b;
+  EXPECT_EQ(a, 2);
+  b *= 10;
+  EXPECT_EQ(b, 8);
+  b *= z;
+  EXPECT_EQ(b, z);
+}
+
+TEST(Arithmetic, Division) {
+  ntlib::rational a(5, 2);
+  ntlib::rational b(4, 5);
+  ntlib::rational z(0, 2);
+
+  EXPECT_DEATH(a / z, "");
+  EXPECT_EQ(a / b, ntlib::rational(25, 8));
+  EXPECT_EQ(b / a, ntlib::rational(8, 25));
+  a /= b;
+  EXPECT_EQ(a, ntlib::rational(25, 8));
+  b /= 10;
+  EXPECT_EQ(b, ntlib::rational(2, 25));
+  EXPECT_DEATH(b /= z, "");
+}
