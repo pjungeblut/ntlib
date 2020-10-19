6089dddf14bdd5d536630d64c11fea5325cedc9e
Paul Jungeblut <paul.jungeblut@gmail.com>
2020-10-19 18:56:56 +0200

    Current code for prime generation.

--- include/prime_generation.hpp
+++ include/prime_generation.hpp
@@ -19,15 +19,46 @@ namespace ntlib {
  * @param sieve The vector to be used as a sieve.
  */
 template<typename T>
-void eratosthenes_sieve(T N, std::vector<bool> &sieve) {
+void eratosthenes_sieve_classic(T N, std::vector<bool> &sieve) {
   sieve.assign(N + 1, true);
   sieve[0] = 0;
   if (N >= 1) sieve[1] = 0;
   for (T i = 4; i <= N; i += 2) sieve[i] = 0;
-  for (T i = 3; i * i <= N; i += 2) {
+  for (T i = 5; i * i <= N; i += 2) {
     if (sieve[i]) {
-      for (T j = i * i; j <= N; j += i) sieve[j] = 0;
+      for (T j = i * i; j <= N; j += 2 * i) sieve[j] = 0;
     }
+  }
+}
+
+/**
+ * Identify all prime numbers up to N.
+ * Runtime: O(N log log N)
+ *
+ * @param N Identify all primes up to N.
+ * @param sieve The vector to be used as a sieve.
+ */
+template<typename T>
+void eratosthenes_sieve(T N, std::vector<bool> &sieve) {
+  sieve.clear();
+  sieve.reserve(N + 1);
+  sieve.push_back(0); // 0
+  sieve.push_back(0); // 1
+  sieve.push_back(1); // 2
+  sieve.push_back(1); // 3
+  sieve.push_back(0); // 4
+  for (std::size_t i = 5; i <= N; i += 6) {
+    sieve.push_back(1);
+    sieve.push_back(0);
+    sieve.push_back(1);
+    sieve.push_back(0);
+    sieve.push_back(0);
+    sieve.push_back(0);
+  }
+  for (T i = 5; i * i <= N; i += 4) {
+    if (sieve[i]) for (T j = i * i; j <= N; j += 2 * i) sieve[j] = 0;
+    i += 2;
+    if (sieve[i]) for (T j = i * i; j <= N; j += 2 * i) sieve[j] = 0;
   }
 }
 
@@ -94,6 +125,37 @@ void eratosthenes_list(T N, std::vector<T> &primes) {
       if (sieve[i - mini]) primes.push_back(i);
     }
   }
+}
+
+/**
+ * Identify all prime numbers up to N using the sieve of Sundaram.
+ * Runtime: O(N log log N)
+ *
+ * Slow and it is easy to get overflows.
+ *
+ * @param N Identify all primes up to N.
+ * @param sieve The vector to be used as a sieve.
+ */
+template<typename T>
+void sundaram_sieve(T N, std::vector<bool> &sieve) {
+  T K = (N - 2) / 2;
+  sieve.reserve(N + 1);
+  sieve.assign(K + 1, true);
+  sieve.resize(N + 1, false);
+
+  for (T i = 1; i <= K; ++i) {
+    for (T j = i; i + j + 2 * i * j <= K; ++j) {
+      sieve[i + j + 2 * i * j] = false;
+    }
+  }
+  for (T i = K; i > 0; --i) {
+    if (sieve[i]) {
+      sieve[2 * i + 1] = true;
+      sieve[i] = false;
+    }
+  }
+  sieve[0] = 0;
+  if (N >= 2) sieve[2] = 1;
 }
 
 }
--- test/prime_generation.cpp
+++ test/prime_generation.cpp
@@ -6,8 +6,8 @@
 #include "prime_generation.hpp"
 #include "prime_test.hpp"
 
-const uint32_t N = 1'000'000;
-const uint32_t M = 1'000'000'000;
+const uint64_t N = 1'000'000;
+const uint64_t M = 1'000'000'000;
 
 TEST(Eratosthenes, Sieve) {
   std::vector<bool> sieve;
@@ -19,27 +19,27 @@ TEST(Eratosthenes, Sieve) {
 
 TEST(Eratosthenes, SieveList) {
   std::vector<bool> sieve;
-  std::vector<uint32_t> primes;
+  std::vector<uint64_t> primes;
   ntlib::eratosthenes_sieve_list(N, sieve, primes);
-  uint32_t counter = 0;
-  for (uint32_t i = 0; i <= N; ++i) {
+  uint64_t counter = 0;
+  for (uint64_t i = 0; i <= N; ++i) {
     counter += sieve[i];
     EXPECT_EQ(sieve[i], ntlib::is_prime_naive(i));
   }
 
   EXPECT_EQ(primes.size(), counter);
-  for (uint32_t p : primes) EXPECT_TRUE(ntlib::is_prime_naive(p));
+  for (uint64_t p : primes) EXPECT_TRUE(ntlib::is_prime_naive(p));
 }
 
 TEST(Eratosthenes, List) {
-  std::vector<uint32_t> primes;
+  std::vector<uint64_t> primes;
   ntlib::eratosthenes_list(N, primes);
-  for (uint32_t p : primes) EXPECT_TRUE(ntlib::is_prime_naive(p));
+  for (uint64_t p : primes) EXPECT_TRUE(ntlib::is_prime_naive(p));
 }
 
 TEST(Eratosthenes, VariantsMatch) {
   std::vector<bool> sieve1, sieve2;
-  std::vector<uint32_t> list1, list2;
+  std::vector<uint64_t> list1, list2;
 
   ntlib::eratosthenes_sieve(M, sieve1);
   ntlib::eratosthenes_sieve_list(M, sieve2, list1);
@@ -47,4 +47,21 @@ TEST(Eratosthenes, VariantsMatch) {
 
   EXPECT_EQ(sieve1, sieve2);
   EXPECT_EQ(list1, list2);
+}
+
+TEST(Sundaram, Sieve) {
+  std::vector<bool> sieve;
+  ntlib::sundaram_sieve(N, sieve);
+  for (uint64_t i = 0; i <= N; ++i) {
+    EXPECT_EQ(sieve[i], ntlib::is_prime_naive(i)) << " at value " << i;
+  }
+}
+
+TEST(Sieves, VariantsMatch) {
+  std::vector<bool> sieve1, sieve2;
+
+  ntlib::eratosthenes_sieve(M, sieve1);
+  ntlib::sundaram_sieve(M, sieve2);
+
+  EXPECT_EQ(sieve1, sieve2);
 }
