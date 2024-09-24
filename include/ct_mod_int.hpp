7ed9be57d955f4233b1a3219c8304726fea156c0
Paul Jungeblut <paul.jungeblut@gmail.com>
2024-09-24 11:01:06 +0200

    Adding type for elements in a residue class (compile time) .

---
+++ include/ct_mod_int.hpp
@@ -0,0 +1,209 @@
+#pragma once
+
+#include <istream>
+#include <ostream>
+#include <type_traits>
+
+#include "modulo.hpp"
+
+namespace ntlib {
+
+/**
+ * Class to represent an element of residue class Z/modulusZ.
+ * Here, `modulus` is a compile time constant.
+ * 
+ * The underlying (unsigned) datatype needs to be big enough to hold
+ * `modulus^2` to work correctly.
+ */
+template <typename U, U modulus, typename S = std::make_signed<U>>
+class ct_mod_int {
+public:
+  /**
+   * Constructs an element of the residue class Z/modulusZ.
+   * 
+   * @param value The initial value.
+   */
+  ct_mod_int(U value = 0) : value(mod(value, modulus)) {
+    static_assert(modulus > 0);
+  }
+
+  /**
+   * Returns the value.
+   * 
+   * @return The value in range `[0,modulus)`.
+   */
+  [[nodiscard]]
+  U get() const {
+    return value;
+  }
+
+  /**
+   * Sets a value.
+   * 
+   * @param v The value.
+   */
+  void set(U v) {
+    value = mod(v, modulus);
+  }
+
+  /**
+   * Prefix increment.
+   * 
+   * @return Reference to this `ct_mod_int` (with the new value).
+   */
+  ct_mod_int& operator++() {
+    value = mod(value + 1, modulus);
+    return *this;
+  }
+
+  /**
+   * Postfix increment.
+   * 
+   * @param _ To indicate postfix version.
+   * @return A `ct_mod_int` with the old value.
+   */
+  ct_mod_int operator++(int) {
+    ct_mod_int old = *this;
+    operator++();
+    return old;
+  }
+
+  /**
+   * Prefix decrement.
+   * 
+   * @return Reference to this `ct_mod_int` (with the new value).
+   */
+  ct_mod_int& operator--() {
+    value = value ? value - 1 : modulus - 1;
+    return *this;
+  }
+
+  /**
+   * Postfix decrement.
+   * 
+   * @param _ To indicate postfix version.
+   * @return A `ct_mod_int` with the old value.
+   */
+  ct_mod_int operator--(int) {
+    ct_mod_int old = *this;
+    operator--();
+    return old;
+  }
+
+  /**
+   * Compound addition.
+   * 
+   * @param rhs The summand.
+   * @return Reference to this `ct_mod_int`.
+   */
+  ct_mod_int& operator+=(ct_mod_int rhs) {
+    value = mod(value + rhs.value, modulus);
+    return *this;
+  }
+
+  /**
+   * Addition.
+   * 
+   * @param lhs The first summand.
+   * @param rhs The second summand.
+   * @return The sum.
+   */
+  friend ct_mod_int operator+(ct_mod_int lhs, ct_mod_int rhs) {
+    lhs += rhs;
+    return lhs;
+  }
+
+  /**
+   * Compound subtraction.
+   * 
+   * @param rhs The subtrahend.
+   * @return Reference to this `ct_mod_int`.
+   */
+  ct_mod_int& operator-=(ct_mod_int rhs) {
+    // Adding `modulus` to avoid negative intermediate values.
+    value = mod(value + modulus - rhs.value, modulus);
+    return *this;
+  }
+  
+  /**
+   * Subtraction.
+   * 
+   * @param lhs The minuend.
+   * @param rhs The subtrahend.
+   * @return The difference.
+   */
+  friend ct_mod_int operator-(ct_mod_int lhs, ct_mod_int rhs) {
+    lhs -= rhs;
+    return lhs;
+  }
+
+  /**
+   * Compound multiplication.
+   * 
+   * @param rhs The second factor.
+   * @return Reference to this `ct_mod_int`.
+   */
+  ct_mod_int& operator*=(ct_mod_int rhs) {
+    value = mod(value * rhs.value, modulus);
+    return *this;
+  }
+
+  /**
+   * Multiplication.
+   * 
+   * @param lhs The first factor.
+   * @param rhs The second factor.
+   * @return The product.
+   */
+  friend ct_mod_int operator*(ct_mod_int lhs, ct_mod_int rhs) {
+    lhs *= rhs;
+    return lhs;
+  }
+
+  /**
+   * Equality comparison.
+   * Compiler will automatically generate `operator!=`.
+   * 
+   * @param lhs The left operand.
+   * @param rhs The right operand.
+   * @return Whether they are equal.
+   */
+  friend bool operator==(ct_mod_int lhs, ct_mod_int rhs) = default;
+
+private:
+  /**
+   * The value in the range `[0,modulus)`.
+   */
+  U value;
+};
+
+/**
+ * Stream insertion operator.
+ * Just prints the value in the range `[0,modulus)`.
+ * 
+ * @param os Reference to the output stream.
+ * @param obj Instance of a `ct_mod_int`.
+ * @return Reference to the output stream.
+ */
+template<typename U, U modulus, typename S = std::make_signed_t<U>>
+std::ostream& operator<<(std::ostream &os, ct_mod_int<U, modulus, S> obj) {
+  os << obj.get();
+  return os;
+}
+
+/**
+ * Stream extraction operator.
+ * 
+ * @param is Reference to the input stream.
+ * @param obj Reference to the `ct_mod_int` to store the read value.
+ * @return Reference to the input stream.
+ */
+template<typename U, U modulus, typename S = std::make_signed_t<U>>
+std::istream& operator>>(std::istream &is, ct_mod_int<U, modulus, S> &obj) {
+  U tmp;
+  is >> tmp;
+  obj.set(tmp);
+  return is;
+}
+
+}
---
+++ test/ct_mod_int.cpp
@@ -0,0 +1,184 @@
+#include <gtest/gtest.h>
+
+#include <cstdint>
+#include <sstream>
+#include <string>
+
+#include "ct_mod_int.hpp"
+
+// Must be at least 1.
+static constexpr uint32_t MOD = 10;
+
+TEST(Check, CompileTimeContants) {
+  static_assert(MOD > 0);
+}
+
+TEST(Construction, Default) {
+  ntlib::ct_mod_int<uint32_t, MOD> a;
+  EXPECT_EQ(a.get(), 0);
+}
+
+TEST(Construction, ByValue) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(0);
+  EXPECT_EQ(a.get(), 0);
+
+  ntlib::ct_mod_int<uint32_t, MOD> b(MOD - 1);
+  EXPECT_EQ(b.get(), MOD - 1);
+
+  ntlib::ct_mod_int<uint32_t, MOD> c(MOD);
+  EXPECT_EQ(c.get(), 0);
+
+  ntlib::ct_mod_int<int32_t, MOD> d(-MOD);
+  EXPECT_EQ(d.get(), 0);
+
+  ntlib::ct_mod_int<int32_t, MOD> e(1 - MOD);
+  EXPECT_EQ(e.get(), 1 % MOD);
+}
+
+TEST(Streaming, Insertion) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(5), b(MOD - 1), c(MOD);
+  std::string result =
+      std::to_string(5 % MOD) + " " + std::to_string(MOD - 1) + " 0";
+  std::ostringstream out;
+  out << a << " " << b << " " << c;
+  EXPECT_EQ(out.str(), result);
+}
+
+TEST(Streaming, Extraction) {
+  std::istringstream in(
+      "5 " + std::to_string(MOD - 1) + " " + std::to_string(MOD));
+  ntlib::ct_mod_int<uint32_t, MOD> a, b, c;
+  in >> a >> b >> c;
+  EXPECT_EQ(a.get(), 5 % MOD);
+  EXPECT_EQ(b.get(), MOD - 1);
+  EXPECT_EQ(c.get(), 0);
+}
+
+TEST(Increment, Prefix) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(0);
+  EXPECT_EQ((++a).get(), 1 % MOD);
+  EXPECT_EQ(a.get(), 1 % MOD);
+
+  ntlib::ct_mod_int<uint32_t, MOD> b(MOD - 1);
+  EXPECT_EQ((++b).get(), 0);
+  EXPECT_EQ(b.get(), 0);
+}
+
+TEST(Increment, Postfix) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(0);
+  EXPECT_EQ((a++).get(), 0);
+  EXPECT_EQ(a.get(), 1);
+
+  ntlib::ct_mod_int<uint32_t, MOD> b(MOD - 1);
+  EXPECT_EQ((b++).get(), MOD - 1);
+  EXPECT_EQ(b.get(), 0);
+}
+
+TEST(Decrement, Prefix) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(0);
+  EXPECT_EQ((--a).get(), MOD - 1);
+  EXPECT_EQ(a.get(), MOD - 1);
+
+  ntlib::ct_mod_int<uint32_t, MOD> b(1);
+  EXPECT_EQ((--b).get(), 0);
+  EXPECT_EQ(b.get(), 0);
+}
+
+TEST(Decrement, Postfix) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(0);
+  EXPECT_EQ((a--).get(), 0);
+  EXPECT_EQ(a.get(), MOD - 1);
+
+  ntlib::ct_mod_int<uint32_t, MOD> b(1);
+  EXPECT_EQ((b--).get(), 1 % MOD);
+  EXPECT_EQ(b.get(), 0);
+}
+
+TEST(Addition, Compound) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(0), b(1);
+  EXPECT_EQ((a += b).get(), 1 % MOD);
+  EXPECT_EQ(a.get(), 1 % MOD);
+}
+
+TEST(Addition, Symmetric) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(1), b(2);
+  EXPECT_EQ((a + b).get(), (b + a).get());
+}
+
+TEST(Addition, Chained) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(1), b(2), c(3);
+  EXPECT_EQ((a + b + c).get(), 6 % MOD);
+}
+
+TEST(Addition, WithInts) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(1);
+  EXPECT_EQ((a + 4).get(), 5 % MOD);
+  EXPECT_EQ((4 + a).get(), 5 % MOD);
+}
+
+TEST(Subtraction, Compound) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(0), b(1);
+  EXPECT_EQ((a -= b).get(), MOD - 1);
+  EXPECT_EQ(a.get(), MOD - 1);
+
+  ntlib::ct_mod_int<uint32_t, MOD> c(MOD - 1), d(1);
+  EXPECT_EQ((c -= d).get(), (2 * MOD - 2) % MOD);
+  EXPECT_EQ(c.get(), (2 * MOD - 2) % MOD);
+}
+
+TEST(Subtraction, Basic) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(1), b(2);
+  EXPECT_EQ((a - b).get(), MOD - 1);
+  EXPECT_EQ((b - a).get(), 1 % MOD);
+}
+
+TEST(Subtraction, Chained) {
+  static_assert(MOD > 6);
+  ntlib::ct_mod_int<uint32_t, MOD> a(1), b(2), c(3);
+  EXPECT_EQ((a - b - c).get(), 6);
+}
+
+TEST(Subtraction, WithInts) {
+  static_assert(MOD > 7);
+  ntlib::ct_mod_int<uint32_t, MOD> a(1);
+  EXPECT_EQ((a - 4).get(), 7);
+  EXPECT_EQ((4 - a).get(), 3 % MOD);
+}
+
+TEST(Multiplication, Compound) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(2), b(3);
+  EXPECT_EQ((a *= b).get(), 6 % MOD);
+  EXPECT_EQ(a.get(), 6 % MOD);
+}
+
+TEST(Multiplication, Symmetric) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(2), b(3);
+  EXPECT_EQ((a * b).get(), (b * a).get());
+}
+
+TEST(Multiplication, Chained) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(2), b(3), c(4);
+  EXPECT_EQ((a * b * c).get(), 24 % MOD);
+}
+
+TEST(Multiplication, WithInts) {
+  ntlib::ct_mod_int<uint32_t, MOD> a(2);
+  EXPECT_EQ((a * 3).get(), 6 % MOD);
+  EXPECT_EQ((3 * a).get(), 6 % MOD);
+}
+
+TEST(Comparison, Equality) {
+  static_assert(MOD > 2);
+  ntlib::ct_mod_int<uint32_t, MOD> a(1), b(2);
+  EXPECT_TRUE(a == a);
+  EXPECT_FALSE(a == b);
+  EXPECT_TRUE(a + a == b);
+}
+
+TEST(Comparison, Inequality) {
+  static_assert(MOD > 2);
+  ntlib::ct_mod_int<uint32_t, MOD> a(1), b(2);
+  EXPECT_FALSE(a != a);
+  EXPECT_TRUE(a != b);
+  EXPECT_FALSE(a + a != b);
+}
