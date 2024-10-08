58fedf250ad506d1fe70fc4f1839b8b782255c0a
Paul Jungeblut <paul.jungeblut@gmail.com>
2024-10-08 14:00:30 +0200

    Small changes.

--- include/prime_test.hpp
+++ include/prime_test.hpp
@@ -37,7 +37,7 @@ std::optional<bool> is_prime_trial_division(T n,
     if (n == p) { return true; }
     if (n % p == T{0}) { return false; }
     // Known: `n` is not divisible by any prime less than `p`.
-    // Thus: All non-primes until p^2 have already been ruled out.
+    // Thus: All non-primes until `p^2` have already been ruled out.
     if (n <= p * p) { return true; }
   }
 
@@ -230,54 +230,6 @@ bool is_strong_lucas_probable_prime(U n) noexcept {
 
   return false;
 }
-
-// template<typename U, typename S = std::make_signed_t<U>>
-// [[nodiscard]] constexpr
-// bool is_strong_lucas_probable_prime(U n) noexcept {
-//   assert(n > U{2});
-//   assert(is_odd(n));
-
-//   // Find parameters P, Q and D for Lucas sequences.
-//   const std::size_t ITERATIONS_BEFORE_SQUARE_TEST = 10;
-//   S D{5};
-//   bool found_d = false;
-//   for (std::size_t i = 0; i < ITERATIONS_BEFORE_SQUARE_TEST; ++i) {
-//     if (jacobi(D, static_cast<S>(n)) == S{-1}) {
-//       found_d = true;
-//       break;
-//     }
-//     D = D > S{0} ? -(D + S{2}) : -(D - S{2});
-//   }
-//   // If no value for D was found yet, then it might be that n is a perfect
-//   // square. In this case no D exists.
-//   if (!found_d && is_square(n)) { return false; }
-
-//   // If n is not a perfect square we continue looking for a D. It must exist.
-//   while (jacobi(D, static_cast<S>(n)) != S{-1}) {
-//     D = D > S{0} ? -(D + S{2}) : -(D - S{2});
-//   }
-//   const S P{1};
-//   const S Q{(S{1} - D) / S{4}};
-
-//   // Decompose such that `n+1 = o*2^e`.
-//   auto [e, o] = odd_part(n + U{1});
-
-//   // Strong Lucas probable prime test.
-//   auto [u, v] = lucas_nth_term_mod(o, P, Q, static_cast<S>(n));
-//   if (u == S{0} || v == S{0}) { return true; }
-//   while (--e) {
-//     S uu = mod(u * v, static_cast<S>(n));
-//     S vv = v * v + D * u * u;
-//     if (is_odd(vv)) { vv += n; }
-//     vv /= S{2};
-//     vv = mod(vv, static_cast<S>(n));
-//     u = uu;
-//     v = vv;
-//     if (v == S{0}) { return true; }
-//   }
-
-//   return false;
-// }
 
 /**
  * Tests whether a given number `n` is (probable) prime.
