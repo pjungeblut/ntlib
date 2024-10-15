a20c498ba06af31cde807f879599c3405c39c7d7
Paul Jungeblut <paul.jungeblut@gmail.com>
2024-10-15 12:03:09 +0200

    More benchmarking for prime decomposition.

--- benchmarks/experiments/prime_decomposition.hpp
+++ benchmarks/experiments/prime_decomposition.hpp
@@ -1,38 +1,89 @@
 #pragma once
 
-#include "base.hpp"
+#include "../../include/int128.hpp"
+#include "../../include/prime_decomposition.hpp"
+#include "../../include/prime_list.hpp"
 #include "../../include/prime_test.hpp"
+#include "base.hpp"
 
 namespace ntlib {
 namespace experiments {
 
-/**
- * Given an odd composite number `n`, tries to find a non-trivial (possibly
- * non-prime) factor of `n` using Pollard's rho algorithm.
- * Cycle detection by using Floyd's algorithm.
- * 
- * A usual choice for the polynomial function `f` would be:
- * `f(x) = (x^2 + 1) mod n`.
- * 
- * @param n The odd composite.
- * @param f A polynomial function to generate a "pseudorandom" sequence.
- * @param x Initial value for parameter `x`.
- * @return If successful, a non-trivial factor.
- */
 template<typename T, typename F>
 [[nodiscard]] constexpr
-std::optional<T> factor_pollard_rho_floyd(T n, F f, T x) {
-  T y = x;
-  T g = 1;
+std::optional<T> factor_pollard_rho(T n, F f, T x) {
+  assert(!is_prime(n));
 
-  while (d == 1) {
+  T y = x;
+  T g{1};
+
+  while (g == T{1}) {
     x = f(x);
     y = f(f(y));
-    g = gcd(abs(x - y), n);
+    g = gcd(distance(x, y), n);
   }
 
-  if (d == n) { return std::optional<T>(); }
-  else { return d; }
+  if (g == n) { return std::optional<T>(); }
+  else { return g; }
+}
+
+template<typename T, typename F>
+[[nodiscard]] constexpr
+std::optional<T> factor_pollard_rho_brent(T n, F f, T x) {
+  T y = x;
+  T g{1};
+
+  std::size_t i = 1;
+  std::size_t k = 2;
+  while (g == T{1}) {
+    ++i;
+    x = f(x);
+    g = gcd(distance(x, y), n);
+    if (i == k) {
+      y = x;
+      k *= 2;
+    }
+  }
+
+  if (g == n) { return std::optional<T>(); }
+  else { return g; }
+}
+
+template<typename T, typename F>
+[[nodiscard]] constexpr
+std::optional<T> factor_pollard_rho_cpalgos(T n, F f, T x) {
+  T g{1};
+  T q{1};
+  T xs, y;
+
+  std::size_t m = 128;
+  std::size_t l = 1;
+
+  while (g == T{1}) {
+    y = x;
+    for (std::size_t i = 1; i < l; i++) { x = f(x); }
+    std::size_t k = 0;
+    while (k < l && g == T{1}) {
+      xs = x;
+      for (std::size_t i = 0; i < m && i < l - k; ++i) {
+        x = f(x);
+        q = u128{q} * distance(x, y) % n;
+      }
+      g = gcd(q, n);
+      k += m;
+    }
+    l *= 2;
+  }
+
+  if (g == n) {
+    do {
+      xs = f(xs);
+      g = gcd(distance(xs, y), n);
+    } while (g == T{1});
+  }
+
+  if (g == n) { return std::optional<T>(); }
+  else { return g; }
 }
 
 }
