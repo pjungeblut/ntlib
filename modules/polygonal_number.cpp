a5bebc15f0ed029af4e203d2b23bed2dd0ed7eb1
Paul Jungeblut <paul.jungeblut@gmail.com>
2024-10-18 14:26:05 +0200

    Improving code for `polygonal_number` module.

--- modules/polygonal_number.cpp
+++ modules/polygonal_number.cpp
@@ -7,124 +7,135 @@ export module polygonal_number;
 namespace ntlib {
 
 /**
- * Computes the n-th triangle number.
+ * Computes the `n`-th triangle number.
  *
  * @param n Which triangle number to generate.
- * @return The n-th triangle number.
+ * @return The `n`-th triangle number.
  */
 export template<typename T>
-T triangle_number(T n) {
+[[nodiscard]] constexpr
+T triangle_number(T n) noexcept {
   return n * (n + 1) / 2;
 }
 
 /**
- * Computes the n-th square number.
+ * Computes the `n`-th square number.
  *
  * @param n Which square number to generate.
- * @return The n-th square number.
+ * @return The `n`-th square number.
  */
 export template<typename T>
-T square_number(T n) {
+[[nodiscard]] constexpr
+T square_number(T n) noexcept {
   return n * n;
 }
 
 /**
- * Computes the n-th pentagonal number.
+ * Computes the `n`-th pentagonal number.
  *
  * @param n Which pentagonal number to generate.
- * @return The n-th pentagonal number.
+ * @return The `n`-th pentagonal number.
  */
 export template<typename T>
-T pentagonal_number(T n) {
+[[nodiscard]] constexpr
+T pentagonal_number(T n) noexcept {
   return n * (3 * n - 1) / 2;
 }
 
 /**
- * Computes the n-th hexagonal number.
+ * Computes the `n`-th hexagonal number.
  *
  * @param n Which hexagonal number to generate.
- * @return The n-th hexagonal number.
+ * @return The `n`-th hexagonal number.
  */
 export template<typename T>
-T hexagonal_number(T n) {
+[[nodiscard]] constexpr
+T hexagonal_number(T n) noexcept {
   return n * (2 * n - 1);
 }
 
 /**
- * Computes the n-th heptagonal number.
+ * Computes the `n`-th heptagonal number.
  *
  * @param n Which heptagonal number to generate.
- * @return The n-th heptagonal number.
+ * @return The `n`-th heptagonal number.
  */
 export template<typename T>
-T heptagonal_number(T n) {
+[[nodiscard]] constexpr
+T heptagonal_number(T n) noexcept {
   return n * (5 * n - 3) / 2;
 }
 
 /**
- * Computes the n-th octagonal number.
+ * Computes the `n`-th octagonal number.
  *
  * @param n Which octagonal number to generate.
- * @return The n-th octagonal number.
+ * @return The `n`-th octagonal number.
  */
 export template<typename T>
-T octagonal_number(T n) {
+[[nodiscard]] constexpr
+T octagonal_number(T n) noexcept {
   return n * (3 * n - 2);
 }
 
 /**
- * Computes the n-th tetrahedral number.
+ * Computes the `n`-th tetrahedral number.
  *
  * @param n Which tetrahedral number to generate.
- * @return The n-th tetrahedral number.
+ * @return The `n`-th tetrahedral number.
  */
 export template<typename T>
-T tetrahedral_number(T n) {
+[[nodiscard]] constexpr
+T tetrahedral_number(T n) noexcept {
   return n * (n + 1) / 2 * (n + 2) / 3;
 }
 
 /**
- * Computes the n-th cube number.
+ * Computes the `n`-th cube number.
  *
  * @param n Which cube number to generate.
- * @return The n-th cube number.
+ * @return The `n`-th cube number.
  */
 export template<typename T>
-T cube_number(T n) {
+[[nodiscard]] constexpr
+T cube_number(T n) noexcept {
   return n * n * n;
 }
 
 /**
- * Computes the n-th octahedral number.
+ * Computes the `n`-th octahedral number.
  *
  * @param n Which octahedral number to generate.
- * @return The n-th octahedral number.
+ * @return The `n`-th octahedral number.
  */
 export template<typename T>
-T octahedral_number(T n) {
+[[nodiscard]] constexpr
+T octahedral_number(T n) noexcept {
   return n * (2 * n * n + 1) / 3;
 }
 
 /**
- * Computes the n-th square pyramid number.
+ * Computes the `n`-th square pyramid number.
  *
  * @param n Which square pyramid number to gnerate.
- * @return The n-th square pyramid number.
+ * @return The `n`-th square pyramid number.
  */
 export template<typename T>
-T square_pyramid_number(T n) {
+[[nodiscard]] constexpr
+T square_pyramidal_number(T n) noexcept{
   return n * (n + 1) / 2 * (2 * n + 1) / 3;
 }
 
 /**
- * Computes the n-th centered square number.
+ * Computes the `n`-th centered square number.
  *
  * @param n Which centered square number to gnerate.
- * @return The n-th centered square number.
+ * @return The `n`-th centered square number.
  */
 export template<typename T>
-T centered_square_number(T n) {
-  return square_number(n) + square_number(n - 1);
+[[nodiscard]] constexpr
+T centered_square_number(T n) noexcept {
+  return square_number(n + 1) + square_number(n);
 }
 
 }
