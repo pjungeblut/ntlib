2b1d9121d5c13762006b35b33a97fbeef7aa92a9
Paul Jungeblut <paul.jungeblut@gmail.com>
2024-11-13 21:39:21 +0100

    Adding file description for `base.cpp`.

--- modules/base/base.cpp
+++ modules/base/base.cpp
@@ -1,11 +1,11 @@
 /**
- * @file base.cpp
+ * @file
+ * @brief Basic functionality used frequently throughout the whole library
  */
 
 /**
  * @module base
- * @brief Provides basic functionality used frequently throughout the whole
- * library.
+ * @brief Basic functionality used frequently throughout the whole library.
  * 
  * Functionality contained in this module has no further dependencies and only
  * depends on the C++ standard library.
