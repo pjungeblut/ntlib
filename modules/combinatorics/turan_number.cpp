27b92c475e34e0a736a1a28e23de1b2d533d40a6
Paul Jungeblut <paul.jungeblut@gmail.com>
2024-11-23 17:33:09 +0100

    Documentation for module `turan_number`.

--- modules/combinatorics/turan_number.cpp
+++ modules/combinatorics/turan_number.cpp
@@ -1,18 +1,31 @@
+/**
+ * @file
+ * @brief Primary module interface unit for module `turan_number`.
+ */
 module;
 
 #include <cassert>
 
+/**
+ * @module turan_number
+ * @brief Compute the number of edges in a Turan graph.
+ * 
+ * The Turan graph \f$T(n,k)\f$ is a complete \f$k\f$-partite graph on \f$n\f$
+ * vertices with almost equal parts.
+ * By Turan's theorem, the Turan graph \f$T(n,k)\f$ is the unique graph with the
+ * maximum number of edges not containing \f$K_{k+1}\f$ as a subgraph.
+ */
 export module turan_number;
 
 namespace ntlib {
 
 /**
- * Computes the number of edges in the Turan graph `T(n,k)`, that is a complete
- * `k`-partite graph on `n` vertices with almost equal parts.
+ * @brief Computes the number of edges in the Turan graph \f$T(n,k)\f$.
  *
+ * @tparam T An integer-like type.
  * @param n The number of vertices.
  * @param k The number of bipartition sets.
- * @return The number of edges in `T(n,k)`.
+ * @return The number of edges in \f$T(n,k)\f$.
  */
 export template<typename T>
 [[nodiscard]] constexpr
