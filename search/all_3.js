ae0f84fcf1d1bd3133e5e9f9ed2bdda59eaaeda1
pjungeblut <pjungeblut@users.noreply.github.com>
2024-12-23 16:40:46 +0000

    deploy: 9e6e9872b0abc36839c56a25df72d5bb17e7f303

--- search/all_3.js
+++ search/all_3.js
@@ -4,11 +4,14 @@ var searchData=
   ['difference_1',['difference',['../base_8cpp.html#a20812657384d1d4eaa06fb8da9927ee9',1,'ntlib']]],
   ['diophantine_2',['diophantine',['../module__diophantine.html',1,'']]],
   ['diophantine_20equations_3',['Diophantine Equations',['../index.html#autotoc_md5',1,'']]],
-  ['disclaimer_4',['License &amp; Disclaimer',['../md_README.html#autotoc_md17',1,'']]],
-  ['divisor_5ffunction_5',['divisor_function',['../divisors_8cpp.html#a03625781ea45c307bd5e58f28bb1803d',1,'ntlib']]],
-  ['divisors_6',['divisors',['../module__divisors.html',1,'']]],
-  ['divisors_2ecpp_7',['divisors.cpp',['../divisors_8cpp.html',1,'']]],
-  ['documentation_8',['Documentation',['../index.html#autotoc_md0',1,'Documentation'],['../md_README.html#autotoc_md11',1,'Documentation']]],
-  ['dodecahedral_5fnumber_9',['dodecahedral_number',['../figurate__number_8cpp.html#a0284fd78c6b25ec6a3833141ca0795d3',1,'ntlib']]],
-  ['downloading_20ntlib_10',['Downloading NTLib',['../md_README.html#autotoc_md13',1,'']]]
+  ['diophantine_2ecpp_4',['diophantine.cpp',['../diophantine_8cpp.html',1,'']]],
+  ['diophantine_5flinear_5fbivariate_5',['diophantine_linear_bivariate',['../diophantine_8cpp.html#a80be6d6096392bd23dffefdd75708bab',1,'ntlib']]],
+  ['diophantine_5flinear_5funivariate_6',['diophantine_linear_univariate',['../diophantine_8cpp.html#a076993e18fe3aaf372ac2ac2ba54b93e',1,'ntlib']]],
+  ['disclaimer_7',['License &amp; Disclaimer',['../md_README.html#autotoc_md17',1,'']]],
+  ['divisor_5ffunction_8',['divisor_function',['../divisors_8cpp.html#a03625781ea45c307bd5e58f28bb1803d',1,'ntlib']]],
+  ['divisors_9',['divisors',['../module__divisors.html',1,'']]],
+  ['divisors_2ecpp_10',['divisors.cpp',['../divisors_8cpp.html',1,'']]],
+  ['documentation_11',['Documentation',['../index.html#autotoc_md0',1,'Documentation'],['../md_README.html#autotoc_md11',1,'Documentation']]],
+  ['dodecahedral_5fnumber_12',['dodecahedral_number',['../figurate__number_8cpp.html#a0284fd78c6b25ec6a3833141ca0795d3',1,'ntlib']]],
+  ['downloading_20ntlib_13',['Downloading NTLib',['../md_README.html#autotoc_md13',1,'']]]
 ];
--- search/files_2.js
+++ search/files_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['divisors_2ecpp_0',['divisors.cpp',['../divisors_8cpp.html',1,'']]]
+  ['diophantine_2ecpp_0',['diophantine.cpp',['../diophantine_8cpp.html',1,'']]],
+  ['divisors_2ecpp_1',['divisors.cpp',['../divisors_8cpp.html',1,'']]]
 ];
