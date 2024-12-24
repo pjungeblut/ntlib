be5fb074b50631c47921485a9229e40fa122b504
pjungeblut <pjungeblut@users.noreply.github.com>
2024-12-24 12:53:43 +0000

    deploy: 7d86af16f7b0eb71d8eeca970dd4cd9cb15fcd05

--- search/functions_c.js
+++ search/functions_c.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['next_5fpell_5fsolution_0',['next_pell_solution',['../pell__equation_8cpp.html#a9a5f2cfa0956c4f425aacd74de894d6c',1,'ntlib']]],
-  ['next_5fprime_1',['next_prime',['../prime__generation_8cpp.html#aeec5186f51aaee09f35aa65075938a02',1,'ntlib']]],
-  ['nth_5fconvergent_5fquadratic_5firrational_5fcf_2',['nth_convergent_quadratic_irrational_cf',['../continued__fraction_8cpp.html#abfcf8d37cb1c6c257b4076647c01191b',1,'ntlib']]]
+  ['next_5fprime_0',['next_prime',['../prime__generation_8cpp.html#aeec5186f51aaee09f35aa65075938a02',1,'ntlib']]],
+  ['nth_5fconvergent_5fquadratic_5firrational_5fcf_1',['nth_convergent_quadratic_irrational_cf',['../continued__fraction_8cpp.html#abfcf8d37cb1c6c257b4076647c01191b',1,'ntlib']]]
 ];
--- search/functions_e.js
+++ search/functions_e.js
@@ -1,12 +1,13 @@
 var searchData=
 [
   ['pell_5ffundamental_5fsolution_0',['pell_fundamental_solution',['../pell__equation_8cpp.html#aa51da102a74a2bce7f08e4b8d049b699',1,'ntlib']]],
-  ['pow_1',['pow',['../base_8cpp.html#a9493740aa48e914151a1049c96cbe16d',1,'ntlib']]],
-  ['prime_5fdecomposition_2',['prime_decomposition',['../prime__decomposition_8cpp.html#a4acaba006b0c7a90e2c412847bd40333',1,'ntlib']]],
-  ['prime_5fdecomposition_5f32_3',['prime_decomposition_32',['../prime__decomposition_8cpp.html#a593f89d6f0bd72904c7bfdf457e7496a',1,'ntlib']]],
-  ['prime_5fdecomposition_5flarge_4',['prime_decomposition_large',['../prime__decomposition_8cpp.html#aa1852fd43ee2b9d00a86dfbea5324223',1,'ntlib']]],
-  ['prime_5fdecomposition_5flist_5',['prime_decomposition_list',['../prime__decomposition_8cpp.html#a74664a0975d26fbbf2ca79c0dadd5bce',1,'ntlib']]],
-  ['prime_5fdecomposition_5flist_5fremainder_6',['prime_decomposition_list_remainder',['../prime__decomposition_8cpp.html#a8f79314e1e8b0c8acaeb3df8490acf86',1,'ntlib']]],
-  ['prime_5fsieve_7',['prime_sieve',['../prime__generation_8cpp.html#a2f3174dcb0f5daf239e101ba2ebd7c70',1,'ntlib::prime_sieve(T N)'],['../prime__generation_8cpp.html#a1eb5039614a4caba9be15ce75355cb87',1,'ntlib::prime_sieve(T N, std::vector&lt; T &gt; &amp;primes)']]],
-  ['primitive_5fpythagorean_5ftriples_8',['primitive_pythagorean_triples',['../pythagorean__triple_8cpp.html#a964f287582daee6f425e736bacf83df3',1,'ntlib']]]
+  ['pell_5fnext_5fsolution_1',['pell_next_solution',['../pell__equation_8cpp.html#a61bb81145969b479f50d7c0069bf2f2b',1,'ntlib']]],
+  ['pow_2',['pow',['../base_8cpp.html#a9493740aa48e914151a1049c96cbe16d',1,'ntlib']]],
+  ['prime_5fdecomposition_3',['prime_decomposition',['../prime__decomposition_8cpp.html#a4acaba006b0c7a90e2c412847bd40333',1,'ntlib']]],
+  ['prime_5fdecomposition_5f32_4',['prime_decomposition_32',['../prime__decomposition_8cpp.html#a593f89d6f0bd72904c7bfdf457e7496a',1,'ntlib']]],
+  ['prime_5fdecomposition_5flarge_5',['prime_decomposition_large',['../prime__decomposition_8cpp.html#aa1852fd43ee2b9d00a86dfbea5324223',1,'ntlib']]],
+  ['prime_5fdecomposition_5flist_6',['prime_decomposition_list',['../prime__decomposition_8cpp.html#a74664a0975d26fbbf2ca79c0dadd5bce',1,'ntlib']]],
+  ['prime_5fdecomposition_5flist_5fremainder_7',['prime_decomposition_list_remainder',['../prime__decomposition_8cpp.html#a8f79314e1e8b0c8acaeb3df8490acf86',1,'ntlib']]],
+  ['prime_5fsieve_8',['prime_sieve',['../prime__generation_8cpp.html#a2f3174dcb0f5daf239e101ba2ebd7c70',1,'ntlib::prime_sieve(T N)'],['../prime__generation_8cpp.html#a1eb5039614a4caba9be15ce75355cb87',1,'ntlib::prime_sieve(T N, std::vector&lt; T &gt; &amp;primes)']]],
+  ['primitive_5fpythagorean_5ftriples_9',['primitive_pythagorean_triples',['../pythagorean__triple_8cpp.html#a964f287582daee6f425e736bacf83df3',1,'ntlib']]]
 ];
