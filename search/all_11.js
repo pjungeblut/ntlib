9f10fc3bec2723905f468f94ba17b45f9ac7d256
pjungeblut <pjungeblut@users.noreply.github.com>
2025-10-25 18:50:45 +0000

    deploy: 9d3392cb244a03ad6df2c7a3f92573bde026176f

--- search/all_11.js
+++ search/all_11.js
@@ -3,7 +3,8 @@ var searchData=
   ['rational_0',['rational',['../classntlib_1_1rational.html',1,'ntlib::rational&lt; T &gt;'],['../module__rational.html',1,'rational module'],['../classntlib_1_1rational.html#a01974d998601167751587de968563047',1,'ntlib::rational::rational()']]],
   ['rational_2ecpp_1',['rational.cpp',['../rational_8cpp.html',1,'']]],
   ['reference_2',['reference',['../classntlib_1_1sieve__235_1_1reference.html',1,'ntlib::sieve_235']]],
-  ['requirements_3',['Requirements',['../md_README.html#autotoc_md13',1,'']]],
-  ['ring_4',['Ring',['../conceptntlib_1_1Ring.html',1,'ntlib']]],
-  ['run_20tests_20for_20individual_20modules_5',['Build and Run Tests for Individual Modules',['../md_README.html#autotoc_md17',1,'']]]
+  ['reports_3',['Generate Code Coverage Reports',['../md_README.html#autotoc_md18',1,'']]],
+  ['requirements_4',['Requirements',['../md_README.html#autotoc_md13',1,'']]],
+  ['ring_5',['Ring',['../conceptntlib_1_1Ring.html',1,'ntlib']]],
+  ['run_20tests_20for_20individual_20modules_6',['Build and Run Tests for Individual Modules',['../md_README.html#autotoc_md17',1,'']]]
 ];
