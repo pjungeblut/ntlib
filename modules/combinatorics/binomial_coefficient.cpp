7a4efc2f51ccad96429b6eb991d764f992ca22c4
Paul Jungeblut <paul.jungeblut@gmail.com>
2026-05-29 18:31:37 +0200

    Remove redundant assert

--- modules/combinatorics/binomial_coefficient.cpp
+++ modules/combinatorics/binomial_coefficient.cpp
@@ -221,7 +221,6 @@ std::vector<std::vector<T>> binom_table(T N) {
 export template<Integer T>
 [[nodiscard]] constexpr
 std::vector<std::vector<T>> mod_binom_table(std::size_t N, T m) {
-  assert(N >= 0);
   assert(m > T{0});
 
   std::vector<std::vector<T>> binoms(N + 1, std::vector<T>(N + 1, T{0}));
